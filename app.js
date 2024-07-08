861805de95e6a84b932e6ace1ed57f42c3f47c05
hcky1999 <40015951+hcky1999@users.noreply.github.com>
2024-07-08 10:12:16 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World, HECKY LADIV!'));
+app.get('/', (req, res) => res.send('Hello World, HECKY LADIV! AWS CONTINOUS PIPELINE'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
